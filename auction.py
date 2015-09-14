a4f57fe72f20e43b6b1ef3ed5b41ee0c84fcd492
Usiel Riedl <usiel.riedl@gmx.de>
2015-09-14 19:23:09 +0200

    Adapted to multi-unit-auction

--- auction.py
+++ auction.py
@@ -7,40 +7,31 @@ __author__ = 'Usiel'
 
 epsilon = 0.0001
 
+
 class Auction:
-    def __init__(self, items, agents):
-        self.items = items
+    def __init__(self, supply, agents):
+        self.supply = supply
         self.agents = agents
-        self.approximator = NisanDemandQueryApproximator(self.items, self.agents)
-        self.b = [(1./self.approximator.gap) for i in range(0, len(self.items) + len(self.agents))]
-        self.solver = BendersSolver(self.b, self.items, self.agents)
+        self.approximator = LaviSwamyGreedyApproximator(self.supply, self.agents)
+        self.b = [(1. / self.approximator.gap) for i in range(0, len(self.agents))]
+        self.b.append(self.supply / self.approximator.gap)
+        self.solver = BendersSolver(self.b, self.agents)
         self.allocations = []
-
-#    @property
-#    def prices(self):
-#        return self.w[1:len(self.items)+1]
-
-#    @property
-#    def utilities(self):
-#        return self.w[len(self.items)-1:]
 
     def iterate(self):
         self.solver.optimize()
-        allocation = self.approximator.approximate(self.solver.prices, self.solver.utilities)
-        phi = sum([(-1)*w*b for w,b in zip(self.solver.prices.values() + self.solver.utilities.values(), self.b)])
-        secondTerm = 0
+        allocation = self.approximator.approximate(self.solver.price, self.solver.utilities)
+        first_term = sum([-w * b for w, b in zip(self.solver.utilities.values() + [self.solver.price], self.b)])
+        second_term = 0
         for assignment in allocation:
-            #assignment[0] = items
-            #assignment[1][0] = agent.id
-            #assignment[1][1] = v_i(item)
-            for item in assignment.items:
-                secondTerm += self.solver.prices[item]
-            secondTerm += self.solver.utilities[assignment.agentId]
-            secondTerm -= assignment.valuation
-        print 'phi %s' % (phi + secondTerm)
+            second_term += self.solver.price * assignment.quantity
+            second_term += -self.solver.utilities[assignment.agent_id]
+            second_term += assignment.valuation
+        phi = first_term - second_term
+        print 'phi = %s - %s = %s' % (first_term, second_term, phi)
 
         # check if phi with current result of master-problem is z
-        if math.fabs(phi + secondTerm + self.solver.z.x) < epsilon:
+        if phi >= self.solver.z.x:
             self.printResults()
             return False
         # otherwise continue and add cut based on this iteration's allocation
@@ -51,46 +42,41 @@ class Auction:
 
     def printResults(self):
         for index, alloc in enumerate(self.allocations, start=1):
-            print 'X%s:' % index,
+            print 'X%s:' % index
             for assignment in alloc:
-                print '%s <- ' % assignment.agentId,
-                for item in assignment.items:
-                    print '%s, ' % item,
+                assignment.print_me()
             print ''
 
 
-
 class BendersSolver:
-    def __init__(self, b, items, agents):
+    def __init__(self, b, agents):
         self.m = Model("master-problem")
         self.z = self.m.addVar(lb=-GRB.INFINITY, ub=GRB.INFINITY, name="z")
         self.b = b
-        self.items = items
         self.agents = agents
 
-        self.priceVars = dict()
-        for item in self.items:
-            self.priceVars[item] = self.m.addVar(lb=-GRB.INFINITY, ub=0, name="p_%s" % item)
+        self.price_var = dict()
+        self.price_var = self.m.addVar(lb=-GRB.INFINITY, ub=0, name="price")
         self.utilityVars = dict()
         for agent in self.agents:
             self.utilityVars[agent.id] = self.m.addVar(lb=-GRB.INFINITY, ub=0, name="u_%s" % agent.id)
 
         self.m.update()
 
-        #Initial constraints for empty allocation
-        self.m.addConstr(self.z, GRB.LESS_EQUAL, LinExpr(self.b, self.priceVars.values() + self.utilityVars.values()), name="X0")
+        # Initial constraints for empty allocation
+        self.m.addConstr(self.z, GRB.LESS_EQUAL, LinExpr(self.b, self.utilityVars.values() + [self.price_var]),
+                         name="X0")
         self.m.setObjective(self.z, GRB.MAXIMIZE)
 
     @property
-    def prices(self):
-        return dict((v[0], v[1].x) for v in self.priceVars.iteritems())
+    def price(self):
+        return math.fabs(self.price_var.x)
 
     @property
     def utilities(self):
-        return dict((v[0], v[1].x) for v in self.utilityVars.iteritems())
+        return dict((v[0], math.fabs(v[1].x)) for v in self.utilityVars.iteritems())
 
     def optimize(self):
-        #self.m.update()
         self.m.optimize()
         for v in self.m.getVars():
             print('%s %g' % (v.varName, v.x))
@@ -101,108 +87,116 @@ class BendersSolver:
 
     # adds another cut z <= wb - ((c + wA)* X)
     def addBendersCut(self, allocation, name):
-        #valuations summed up
-        expr = LinExpr(self.b, self.priceVars.values() + self.utilityVars.values())
+        # valuations summed up
+        expr = LinExpr(self.b, self.utilityVars.values() + [self.price_var])
         for a in allocation:
             expr.addConstant(-a.valuation)
-            expr.addTerms(-1, self.utilityVars[a.agentId])
-            for item in a.items:
-                expr.addTerms(-1, self.priceVars[item])
+            expr.addTerms(-1, self.utilityVars[a.agent_id])
+            expr.addTerms(-a.quantity, self.price_var)
 
         self.m.addConstr(self.z, GRB.LESS_EQUAL, expr, name='X%s' % name)
-        #self.m.write("out.lp")
+        # self.m.write("out.lp")
 
-class NisanDemandQueryApproximator:
-    def __init__(self, items, agents):
-        self.items = items
+
+class LaviSwamyGreedyApproximator:
+    def __init__(self, supply, agents):
+        self.supply = supply
         self.agents = agents
 
     @property
     def gap(self):
-        return min(len(self.agents), 4*math.sqrt(len(self.items)))
+        return 2.
 
-    def approximate(self, prices, utilities):
-        itemsPool = self.items[:]
-        agentsPool = self.agents[:]
+    def approximate
+
+    def approximate(self, price, utilities):
+        left_supply = self.supply
+        agents_pool = self.agents[:]
         allocation = []
-        summedValuation = 0
+        summed_valuations = 0
 
-        #as done in Nisan 200x
-        while itemsPool and agentsPool:
-            queryResponses = dict()
-            perItemValues = dict()
-            for agent in agentsPool:
-                demand = agent.queryDemand(prices, itemsPool[:])
+        # as done in Lavi & Swamy 2005
+        while agents_pool and left_supply > 0:
+            query_responses = dict()
+            per_item_values = dict()
+            for agent in agents_pool:
+                demand = agent.query_demand(price, left_supply)
                 if demand:
-                    queryResponses[agent.id] = demand
-                    price = sum([prices[item] for item in demand[1]])
-                    if ((queryResponses[agent.id][0] + utilities[agent.id] + price)) > 0:
-                        perItemValues[agent.id] = (queryResponses[agent.id][0] + utilities[agent.id] + price)/len(queryResponses[agent.id][1])
-            if perItemValues:
-                maximalPerItemValueAgentId = max(perItemValues.iterkeys(), key=(lambda key: perItemValues[key]))
-                for i in queryResponses[maximalPerItemValueAgentId][1]:
-                    itemsPool = [item for item in itemsPool if item != i]
-                agentsPool = [agent for agent in agentsPool if agent.id != maximalPerItemValueAgentId]
-                allocation.append(Assignment(queryResponses[maximalPerItemValueAgentId][1], maximalPerItemValueAgentId, queryResponses[maximalPerItemValueAgentId][0]))
-                summedValuation += queryResponses[maximalPerItemValueAgentId][0]
+                    query_responses[agent.id] = demand
+                    if (query_responses[agent.id].valuation - utilities[agent.id] - query_responses[agent.id].quantity * price) > 0:
+                        per_item_values[agent.id] = (
+                                                    query_responses[agent.id].valuation - utilities[agent.id] - price) / \
+                                                    query_responses[agent.id].quantity
+            if per_item_values:
+                maximal_per_item_value_agent_id = max(per_item_values.iterkeys(),
+                                                      key=(lambda key: per_item_values[key]))
+                left_supply -= query_responses[maximal_per_item_value_agent_id].quantity
+                agents_pool = [agent for agent in agents_pool if agent.id != maximal_per_item_value_agent_id]
+                allocation.append(Assignment(query_responses[maximal_per_item_value_agent_id].quantity,
+                                             maximal_per_item_value_agent_id,
+                                             query_responses[maximal_per_item_value_agent_id].valuation))
+                summed_valuations += query_responses[maximal_per_item_value_agent_id].valuation - utilities[
+                    maximal_per_item_value_agent_id] - price * query_responses[maximal_per_item_value_agent_id].quantity
             else:
                 break
 
-        #check if assigning all items to one agent is better
+        # check if assigning all items to one agent is better
         for agent in self.agents:
-            valuation = agent.valueQuery(self.items[:])
-            if valuation > summedValuation:
-                summedValuation = valuation
-                #allocation = [Assignment(self.items[:], agent.id, valuation)]
-
+            valuation = agent.query_value(self.supply)
+            if valuation.valuation - utilities[agent.id] - self.supply * price > summed_valuations:
+                summed_valuations = valuation.valuation
+                allocation = [Assignment(self.supply, agent.id, valuation.valuation)]
 
         for assignment in allocation:
-            print '%s <- ' % assignment.agentId,
-            for item in assignment.items:
-                print '%s, ' % item,
-        print ''
+            assignment.print_me()
 
         return allocation
 
+
 class Assignment:
-    def __init__(self, items, agentId, valuation):
-        self.items = items
-        self.agentId = agentId
+    def __init__(self, quantity, agent_id, valuation):
+        self.quantity = quantity
+        self.agent_id = agent_id
         self.valuation = valuation
 
+    def print_me(self):
+        print 'Agent %s receives %s item(s)' % (self.agent_id, self.quantity)
+
+
+class Valuation:
+    def __init__(self, quantity, valuation):
+        self.quantity = quantity
+        self.valuation = valuation
+
+
 class Agent:
-    def __init__(self, items, valuations, id):
+    def __init__(self, valuations, id):
         self.id = id
         self.valuations = valuations
 
-    def queryDemand(self, prices, items):
-        bestItemSet = None
-        bestValuePerItem = None
+    def query_demand(self, price, left_supply):
+        best_valuation = None
+        best_value_per_item = None
         for valuation in self.valuations:
-            if all(item in items for item in valuation[0]):
-                valuePerItem = (valuation[1] + sum([prices[i] for i in valuation[0]]))/len(valuation[0])
-                if valuePerItem > bestValuePerItem:
-                    bestItemSet = (valuation[1], valuation[0])
-                    bestValuePerItem = valuePerItem
-        return bestItemSet
+            if valuation.quantity <= left_supply:
+                value_per_item = (valuation.valuation - valuation.quantity * price) / valuation.quantity
+                if value_per_item > 0. and value_per_item > best_value_per_item:
+                    best_valuation = valuation
+                    best_value_per_item = value_per_item
+        return best_valuation
 
-    def valueQuery(self, items):
-        valuation = itertools.ifilter(lambda x: set(x[0]) == set(items), self.valuations).next()
+    def query_value(self, quantity):
+        valuation = itertools.ifilter(lambda x: x.quantity == quantity, self.valuations).next()
         if valuation:
-            return valuation[1]
-        return 0
+            return valuation
+        return None
 
 
-items = ["A", "B"]
-agent1 = Agent(items, [(["A"], 6.), (["B"], 6.), (["A", "B"], 6.)], 1)
-agent2 = Agent(items, [(["A"], 1.), (["B"], 1.), (["A", "B"], 5.)], 2)
-agent3 = Agent(items, [(["A"], 3.), (["B"], 1.), (["A", "B"], 3.)], 3)
-#items = ["A", "B", "C"]
-#agent1 = Agent(items, [(["A"], 6.), (["B"], 6.), (["A", "B"], 6.), (["C"], 6.), (["A", "B", "C"], 6.), (["B", "C"], 6.)], 1)
-#agent2 = Agent(items, [(["A"], 1.), (["B"], 1.), (["A", "B"], 4.), (["C"], 1.), (["A", "B", "C"], 10.), (["B", "C"], 2.)], 2)
-#agent3 = Agent(items, [(["A"], 3.), (["B"], 1.), (["A", "B"], 3.), (["C"], 4.), (["A", "B", "C"], 15.), (["B", "C"], 4.)], 3)
+agent1 = Agent([Valuation(1, 6.), Valuation(2, 6.), Valuation(3, 6.), Valuation(4, 6.)], 1)
+agent2 = Agent([Valuation(1, 1.), Valuation(2, 4.), Valuation(3, 4.), Valuation(4, 6.)], 2)
+agent3 = Agent([Valuation(1, 0.), Valuation(2, 1.), Valuation(3, 1.), Valuation(4, 1.)], 3)
 agents = [agent1, agent2, agent3]
-a = Auction(items, agents)
+a = Auction(4, agents)
 
 flag = True
 while flag:
